e3f331437e2b9142968bbae46981d8fc76cee1f8
Julio <Julio@DESKTOP-RQERK49.mshome.net>
2020-10-02 10:52:43 -0500

    Commit inicial del proyecto

---
+++ src/test/java/com/wolox/jsontest/JsontestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wolox.jsontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsontestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
